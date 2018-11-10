653281efdd84389e62f0c439ae3f0d640386cbed
Matt Schmitt <MattSchmitt@Matts-MacBook-Pro.local>
2018-11-10 10:25:00 -0700

    adding fighter data

---
+++ fighterjets/src/fighterData.js
@@ -0,0 +1,51 @@
+import React  from "react"
+class  FighterData extends React.Component{
+    data= [
+        {
+        name: "F84 Thunderjet",
+        make: "Republic  Aviation",
+        topSpeed: "622 MPH",
+        weapons: [
+                    "6 x .50 in M3 Browning machine guns",
+                    "Up to 4,450 lbs of rockets and bombs including 1 x Mark 7 nuclear bomb"
+                ]
+        },
+        {
+        name: "F86 Sabre",
+        make: "North American Aviation",
+        topSpeed: "685 MPH",
+        weapons: [
+                    " 6 X 0.50 in (12.7 mm) M3 Browning machine guns (1,800 rounds in total)",
+                    "variety of rocket launchers; e.g.: 2 Matra rocket pods with 18 SNEB 68 mm rockets per pod",
+                    "5,300 lb (2,400 kg) of payload on four external hardpoints, bombs were usually mounted on outer two pylons as the inner pairs were plumbed for 2 200 US gallons (760 L) drop tanks which gave the Sabre a more useful range. A wide variety of bombs could be carried (max standard loadout being two 1,000 lb bombs plus two drop tanks), napalm canisters and could have included a tactical nuclear weapon."
+                ]
+        },
+        {
+        name: "F100 Super Sabre",
+        make: "North American Aviation",
+        topSpeed: "864 MPH",
+        weapons: [
+                    "4× 20 mm (0.787 in) Pontiac M39A1 revolver cannon w/ 200 rpg",
+                    "4× AIM-9 Sidewinder",
+                    "2× AGM-12 Bullpup",
+                    "2× or 4× LAU-3/A 2.75 unguided rocket dispenser",
+                    "7,040 lb (3,190 kg) of weapons",
+                    "Mark 7 nuclear bomb",
+                    "Mk 28 nuclear bomb",
+                    "Mk 38 nuclear bomb",
+                    "Mk 43 nuclear bomb",
+                ]
+        },
+        {
+        name: "F4 Phantom 2",
+        make: "McDonald Douglas",
+        topSpeed: "1,473 MPH",
+        weapons: [
+                    "E-model has a 20 mm (0.787 in) M61A1 Vulcan cannon mounted internally under the nose, 640 Rounds",
+                    "Up to 18,650 lb (8,480 kg) of weapons on nine external hardpoints, including general purpose bombs, cluster bombs, TV- and laser-guided bombs, rocket pods, air-to-ground missiles, anti-ship missiles, gun pods, and nuclear weapons. Reconnaissance, targeting, electronic countermeasures and baggage pods, and external fuel tanks may also be carried.",
+                    "4× AGM-62 Walleye",
+                    "4× AIM-9 Sidewinders on wing pylons, Israeli F-4 Kurnass 2000 carried Python-3, Japanese F-4EJ Kai carry AAM-3",
+                ]
+        }
+    ]
+}
